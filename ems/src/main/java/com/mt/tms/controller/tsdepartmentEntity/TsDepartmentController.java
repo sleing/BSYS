774216b5859b178f94ecfe2ad20381304b6d9cff
张海山 <95226742+sleing@users.noreply.github.com>
2024-03-17 23:19:37 +0800

    准备功能：增删查改基本完成

--- ems/src/main/java/com/mt/tms/controller/tsdepartmentEntity/TsDepartmentController.java
+++ ems/src/main/java/com/mt/tms/controller/tsdepartmentEntity/TsDepartmentController.java
@@ -10,176 +10,174 @@ import com.mt.common.core.web.base.PageDTO;
 import com.mt.common.core.web.base.PageResultDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 
 import com.mt.tms.dto.tsdepartmentEntity.TsDepartmentEditDto;
 import com.mt.tms.entity.tsdepartmentEntity.TsDepartment;
 import com.mt.tms.service.tsdepartmentEntity.TsDepartmentService;
-						import com.mt.tms.service.tsteacherEntity.TsTeacherInfoService;
-		import com.mt.tms.service.tsstudentEntity.TsStudentInfoService;
-			import com.mt.tms.service.tsstudentEntity.TsStudentInfoService;
-										
+import com.mt.tms.service.tsteacherEntity.TsTeacherInfoService;
+import com.mt.tms.service.tsstudentEntity.TsStudentInfoService;
+import com.mt.tms.service.tsstudentEntity.TsStudentInfoService;
+
 
 @Api(tags = "部门信息管理")
 @RestController
 @RequestMapping("/api/tms/tsdepartmentEntity/TsDepartment")
 @CrossOrigin(allowCredentials = "true")
 public class TsDepartmentController {
-private static Logger logger = LoggerFactory.getLogger(TsDepartmentController.class);
+    private static Logger logger = LoggerFactory.getLogger(TsDepartmentController.class);
 
 
+    @Autowired
+    private TsDepartmentService tsDepartmentService;
+    @Autowired
+    private TsTeacherInfoService tsTeacherInfoService;
+    @Autowired
+    private TsStudentInfoService tsStudentInfoService;
 
-																					
-								
-											
-																																@Autowired private TsDepartmentService tsDepartmentService;
-		@Autowired private TsTeacherInfoService tsTeacherInfoService;
-		@Autowired private TsStudentInfoService tsStudentInfoService;
-	
-	/**
-	* 根据分页参数查询部门信息管理集合
-	*
-	* @param pageDTO 分页条件
-	*/
-	@PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:view')")
-	@ApiOperation("分页查询部门信息管理")
-	@ApiPageParam
-	@PostMapping("/findTsDepartments")
-	public PageResultDTO findTsDepartments(@RequestBody PageDTO pageDTO){
-		return this.tsDepartmentService.findTsDepartments(pageDTO);
-	}
+    /**
+     * 根据分页参数查询部门信息管理集合
+     *
+     * @param pageDTO 分页条件
+     */
+    @PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:view')")
+    @ApiOperation("分页查询部门信息管理")
+    @ApiPageParam
+    @PostMapping("/findTsDepartments")
+    public PageResultDTO findTsDepartments(@RequestBody PageDTO pageDTO) {
+        return this.tsDepartmentService.findTsDepartments(pageDTO);
+    }
 
-	/**
-	* 根据ID查询指定的部门信息管理
-	*
-	* @param tsDepartmentId Id
-	*/
-	@PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:view')")
-	@ApiOperation("根据id查询部门信息管理")
-	@ApiPageParam
-	@PostMapping("/findTsDepartment")
-	public TsDepartment findTsDepartment(@RequestParam Long tsDepartmentId){
-		return this.tsDepartmentService.findTsDepartment(tsDepartmentId);
-	}
+    /**
+     * 根据ID查询指定的部门信息管理
+     *
+     * @param tsDepartmentId Id
+     */
+    @PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:view')")
+    @ApiOperation("根据id查询部门信息管理")
+    @ApiPageParam
+    @PostMapping("/findTsDepartment")
+    public TsDepartment findTsDepartment(@RequestParam Long tsDepartmentId) {
+        return this.tsDepartmentService.findTsDepartment(tsDepartmentId);
+    }
 
-	/**
-	* 根据ID查询指定的部门信息管理(包含外键名称)
-	*
-	* @param tsDepartmentId Id
-	*/
-	@PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:view')")
-	@ApiOperation("根据ID查询指定的部门信息管理(包含外键名称)")
-	@PostMapping("/findTsDepartmentForView")
-	public TsDepartment findTsDepartmentForView(@RequestParam Long tsDepartmentId){
-		return this.tsDepartmentService.findTsDepartmentWithForeignName(tsDepartmentId);
-	}
+    /**
+     * 根据ID查询指定的部门信息管理(包含外键名称)
+     *
+     * @param tsDepartmentId Id
+     */
+    @PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:view')")
+    @ApiOperation("根据ID查询指定的部门信息管理(包含外键名称)")
+    @PostMapping("/findTsDepartmentForView")
+    public TsDepartment findTsDepartmentForView(@RequestParam Long tsDepartmentId) {
+        return this.tsDepartmentService.findTsDepartmentWithForeignName(tsDepartmentId);
+    }
 
-	/**
-	* 根据ID查询指定的部门信息管理(包含部门信息管理和外键名称)
-	*
-	* @param tsDepartmentId Id
-	*/
-	@PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:view')")
-	@ApiOperation("根据ID查询指定的部门信息管理(包含部门信息管理和外键名称)")
-	@PostMapping("/findTsDepartmentForEdit")
-	public TsDepartmentEditDto findTsDepartmentForEdit(@RequestParam Long tsDepartmentId){
-				TsDepartmentEditDto tsDepartmentEditDto = new TsDepartmentEditDto();
-		tsDepartmentEditDto.setTsDepartment(this.tsDepartmentService.findTsDepartmentWithForeignName(tsDepartmentId));
+    /**
+     * 根据ID查询指定的部门信息管理(包含部门信息管理和外键名称)
+     *
+     * @param tsDepartmentId Id
+     */
+    @PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:view')")
+    @ApiOperation("根据ID查询指定的部门信息管理(包含部门信息管理和外键名称)")
+    @PostMapping("/findTsDepartmentForEdit")
+    public TsDepartmentEditDto findTsDepartmentForEdit(@RequestParam Long tsDepartmentId) {
+        TsDepartmentEditDto tsDepartmentEditDto = new TsDepartmentEditDto();
+        tsDepartmentEditDto.setTsDepartment(this.tsDepartmentService.findTsDepartmentWithForeignName(tsDepartmentId));
 
-		this.prepareTsDepartmentEditDto(tsDepartmentEditDto);
+        this.prepareTsDepartmentEditDto(tsDepartmentEditDto);
 
-		return tsDepartmentEditDto;
-	}
+        return tsDepartmentEditDto;
+    }
 
-	/**
-	* 根据ID查询指定的部门信息管理(只提取ID 和 Name)
-	*
-	* @param tsDepartmentId Id
-	*/
-	@PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:view')")
-	@ApiOperation("根据ID查询指定的部门信息管理(只提取ID 和 Name)")
-	@PostMapping("/findTsDepartmentsWithIdNameById")
-	public TsDepartment findTsDepartmentsWithIdNameById(@RequestParam Long tsDepartmentId){
-	return this.tsDepartmentService.findTsDepartmentsWithIdNameById(tsDepartmentId);
-	}
+    /**
+     * 根据ID查询指定的部门信息管理(只提取ID 和 Name)
+     *
+     * @param tsDepartmentId Id
+     */
+    @PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:view')")
+    @ApiOperation("根据ID查询指定的部门信息管理(只提取ID 和 Name)")
+    @PostMapping("/findTsDepartmentsWithIdNameById")
+    public TsDepartment findTsDepartmentsWithIdNameById(@RequestParam Long tsDepartmentId) {
+        return this.tsDepartmentService.findTsDepartmentsWithIdNameById(tsDepartmentId);
+    }
 
-	/**
-	* 根据名称查询部门信息管理集合(只提取ID 和 Name)
-	*
-	* @param tsDepartmentName 名称
-	*/
-	@PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:view')")
-	@ApiOperation("根据名称查询部门信息管理集合(只提取ID 和 Name)")
-	@PostMapping("/findTsDepartmentsWithIdNameByName")
-	public List<TsDepartment> findTsDepartmentsWithIdNameByName(@RequestParam String tsDepartmentName){
-	return this.tsDepartmentService.findTsDepartmentsWithIdNameByName(tsDepartmentName);
-	}
+    /**
+     * 根据名称查询部门信息管理集合(只提取ID 和 Name)
+     *
+     * @param tsDepartmentName 名称
+     */
+    @PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:view')")
+    @ApiOperation("根据名称查询部门信息管理集合(只提取ID 和 Name)")
+    @PostMapping("/findTsDepartmentsWithIdNameByName")
+    public List<TsDepartment> findTsDepartmentsWithIdNameByName(@RequestParam String tsDepartmentName) {
+        return this.tsDepartmentService.findTsDepartmentsWithIdNameByName(tsDepartmentName);
+    }
 
 
-	/**
-	* 创建新的部门信息管理
-	*
-	*/
-	@PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:add')")
-	@ApiOperation("创建新的部门信息管理")
-	@PostMapping("/createTsDepartment")
-	public TsDepartmentEditDto createTsDepartment(){
-				TsDepartmentEditDto tsDepartmentEditDto = new TsDepartmentEditDto();
-		tsDepartmentEditDto.setTsDepartment(new TsDepartment());
+    /**
+     * 创建新的部门信息管理
+     */
+    @PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:add')")
+    @ApiOperation("创建新的部门信息管理")
+    @PostMapping("/createTsDepartment")
+    public TsDepartmentEditDto createTsDepartment() {
+        TsDepartmentEditDto tsDepartmentEditDto = new TsDepartmentEditDto();
+        tsDepartmentEditDto.setTsDepartment(new TsDepartment());
 
-		this.prepareTsDepartmentEditDto(tsDepartmentEditDto);
-		return tsDepartmentEditDto;
-	}
+        this.prepareTsDepartmentEditDto(tsDepartmentEditDto);
+        return tsDepartmentEditDto;
+    }
 
-	private void prepareTsDepartmentEditDto(TsDepartmentEditDto tsDepartmentEditDto){
-																		        //TODO: 以下代码可以注释掉，此行代码即时加载所有外键对象，以便选择。如果不在此加载，可以在界面上实现延迟加载。如果外键对象超过 500 行，建议采用延迟加载
-		tsDepartmentEditDto.setSupervisorTeaTsTeacherInfos(this.tsTeacherInfoService.findAllTsTeacherInfosWithIdName());
-					        //TODO: 以下代码可以注释掉，此行代码即时加载所有外键对象，以便选择。如果不在此加载，可以在界面上实现延迟加载。如果外键对象超过 500 行，建议采用延迟加载
-		tsDepartmentEditDto.setSupervisorStuTsStudentInfos(this.tsStudentInfoService.findAllTsStudentInfosWithIdName());
-								        //TODO: 以下代码可以注释掉，此行代码即时加载所有外键对象，以便选择。如果不在此加载，可以在界面上实现延迟加载。如果外键对象超过 500 行，建议采用延迟加载
-		tsDepartmentEditDto.setLeaderTsStudentInfos(this.tsStudentInfoService.findAllTsStudentInfosWithIdName());
-																															}
+    private void prepareTsDepartmentEditDto(TsDepartmentEditDto tsDepartmentEditDto) {
+        //TODO: 以下代码可以注释掉，此行代码即时加载所有外键对象，以便选择。如果不在此加载，可以在界面上实现延迟加载。如果外键对象超过 500 行，建议采用延迟加载
+        tsDepartmentEditDto.setSupervisorTeaTsTeacherInfos(this.tsTeacherInfoService.findAllTsTeacherInfosWithIdName());
+        //TODO: 以下代码可以注释掉，此行代码即时加载所有外键对象，以便选择。如果不在此加载，可以在界面上实现延迟加载。如果外键对象超过 500 行，建议采用延迟加载
+        tsDepartmentEditDto.setSupervisorStuTsStudentInfos(this.tsStudentInfoService.findAllTsStudentInfosWithIdName());
+        //TODO: 以下代码可以注释掉，此行代码即时加载所有外键对象，以便选择。如果不在此加载，可以在界面上实现延迟加载。如果外键对象超过 500 行，建议采用延迟加载
+        tsDepartmentEditDto.setLeaderTsStudentInfos(this.tsStudentInfoService.findAllTsStudentInfosWithIdName());
+    }
 
-	/**
-	* 新增保存部门信息管理
-	*
-	* @param tsDepartment 实体对象
-	*/
-	@PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:add')")
-	@ApiOperation("新增保存部门信息管理")
-	@PostMapping("/saveTsDepartment")
-	public TsDepartment saveTsDepartment(@RequestBody TsDepartment tsDepartment){
-		return this.tsDepartmentService.saveTsDepartment(tsDepartment);
-	}
+    /**
+     * 新增保存部门信息管理
+     *
+     * @param tsDepartment 实体对象
+     */
+    @PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:add')")
+    @ApiOperation("新增保存部门信息管理")
+    @PostMapping("/saveTsDepartment")
+    public TsDepartment saveTsDepartment(@RequestBody TsDepartment tsDepartment) {
+        return this.tsDepartmentService.saveTsDepartment(tsDepartment);
+    }
 
-	/**
-	* 修改保存部门信息管理
-	*
-	* @param tsDepartment 实体对象
-	*/
-	@PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:update')")
-	@ApiOperation("修改保存部门信息管理")
-	@PostMapping("/updateTsDepartment")
-	public TsDepartment updateTsDepartment(@RequestBody TsDepartment tsDepartment){
-		return this.tsDepartmentService.updateTsDepartment(tsDepartment);
-	}
+    /**
+     * 修改保存部门信息管理
+     *
+     * @param tsDepartment 实体对象
+     */
+    @PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:update')")
+    @ApiOperation("修改保存部门信息管理")
+    @PostMapping("/updateTsDepartment")
+    public TsDepartment updateTsDepartment(@RequestBody TsDepartment tsDepartment) {
+        return this.tsDepartmentService.updateTsDepartment(tsDepartment);
+    }
 
-	/**
-	* 根据ID删除部门信息管理
-	*
-	* @param tsDepartmentId ID
-	*/
-	@PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:remove')")
-	@ApiOperation("根据ID删除部门信息管理")
-	@PostMapping("/deleteTsDepartment")
-	public void deleteTsDepartment(@RequestParam Long tsDepartmentId){
-		this.tsDepartmentService.deleteTsDepartment(tsDepartmentId);
-	}
-
+    /**
+     * 根据ID删除部门信息管理
+     *
+     * @param tsDepartmentId ID
+     */
+    @PreAuthorize("hasAuthority('tms:tsdepartmentEntity:TsDepartment:remove')")
+    @ApiOperation("根据ID删除部门信息管理")
+    @PostMapping("/deleteTsDepartment")
+    public void deleteTsDepartment(@RequestParam Long tsDepartmentId) {
+        this.tsDepartmentService.deleteTsDepartment(tsDepartmentId);
+    }
 
 
 }
--- ems/src/main/java/com/mt/tms/controller/tsteacherEntity/TsTeacherInfoController.java
+++ ems/src/main/java/com/mt/tms/controller/tsteacherEntity/TsTeacherInfoController.java
@@ -10,172 +10,171 @@ import com.mt.common.core.web.base.PageDTO;
 import com.mt.common.core.web.base.PageResultDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 
 import com.mt.tms.dto.tsteacherEntity.TsTeacherInfoEditDto;
 import com.mt.tms.entity.tsteacherEntity.TsTeacherInfo;
 import com.mt.tms.service.tsteacherEntity.TsTeacherInfoService;
-						import com.mt.tms.service.tsCollegeEntity.TsCollegeService;
-				import com.mt.tms.service.tspositionEntity.TsPositionService;
-						
+import com.mt.tms.service.tsCollegeEntity.TsCollegeService;
+import com.mt.tms.service.tspositionEntity.TsPositionService;
+
 
 @Api(tags = "老师信息管理")
 @RestController
 @RequestMapping("/api/tms/tsteacherEntity/TsTeacherInfo")
 @CrossOrigin(allowCredentials = "true")
 public class TsTeacherInfoController {
-private static Logger logger = LoggerFactory.getLogger(TsTeacherInfoController.class);
+    private static Logger logger = LoggerFactory.getLogger(TsTeacherInfoController.class);
 
 
+    @Autowired
+    private TsTeacherInfoService tsTeacherInfoService;
+    @Autowired
+    private TsCollegeService tsCollegeService;
+    @Autowired
+    private TsPositionService tsPositionService;
 
-																					
-														
-																				@Autowired private TsTeacherInfoService tsTeacherInfoService;
-		@Autowired private TsCollegeService tsCollegeService;
-		@Autowired private TsPositionService tsPositionService;
-	
-	/**
-	* 根据分页参数查询老师信息管理集合
-	*
-	* @param pageDTO 分页条件
-	*/
-	@PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:view')")
-	@ApiOperation("分页查询老师信息管理")
-	@ApiPageParam
-	@PostMapping("/findTsTeacherInfos")
-	public PageResultDTO findTsTeacherInfos(@RequestBody PageDTO pageDTO){
-		return this.tsTeacherInfoService.findTsTeacherInfos(pageDTO);
-	}
+    /**
+     * 根据分页参数查询老师信息管理集合
+     *
+     * @param pageDTO 分页条件
+     */
+    @PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:view')")
+    @ApiOperation("分页查询老师信息管理")
+    @ApiPageParam
+    @PostMapping("/findTsTeacherInfos")
+    public PageResultDTO findTsTeacherInfos(@RequestBody PageDTO pageDTO) {
+        return this.tsTeacherInfoService.findTsTeacherInfos(pageDTO);
+    }
 
-	/**
-	* 根据ID查询指定的老师信息管理
-	*
-	* @param tsTeacherInfoId Id
-	*/
-	@PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:view')")
-	@ApiOperation("根据id查询老师信息管理")
-	@ApiPageParam
-	@PostMapping("/findTsTeacherInfo")
-	public TsTeacherInfo findTsTeacherInfo(@RequestParam Long tsTeacherInfoId){
-		return this.tsTeacherInfoService.findTsTeacherInfo(tsTeacherInfoId);
-	}
+    /**
+     * 根据ID查询指定的老师信息管理
+     *
+     * @param tsTeacherInfoId Id
+     */
+    @PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:view')")
+    @ApiOperation("根据id查询老师信息管理")
+    @ApiPageParam
+    @PostMapping("/findTsTeacherInfo")
+    public TsTeacherInfo findTsTeacherInfo(@RequestParam Long tsTeacherInfoId) {
+        return this.tsTeacherInfoService.findTsTeacherInfo(tsTeacherInfoId);
+    }
 
-	/**
-	* 根据ID查询指定的老师信息管理(包含外键名称)
-	*
-	* @param tsTeacherInfoId Id
-	*/
-	@PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:view')")
-	@ApiOperation("根据ID查询指定的老师信息管理(包含外键名称)")
-	@PostMapping("/findTsTeacherInfoForView")
-	public TsTeacherInfo findTsTeacherInfoForView(@RequestParam Long tsTeacherInfoId){
-		return this.tsTeacherInfoService.findTsTeacherInfoWithForeignName(tsTeacherInfoId);
-	}
+    /**
+     * 根据ID查询指定的老师信息管理(包含外键名称)
+     *
+     * @param tsTeacherInfoId Id
+     */
+    @PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:view')")
+    @ApiOperation("根据ID查询指定的老师信息管理(包含外键名称)")
+    @PostMapping("/findTsTeacherInfoForView")
+    public TsTeacherInfo findTsTeacherInfoForView(@RequestParam Long tsTeacherInfoId) {
+        return this.tsTeacherInfoService.findTsTeacherInfoWithForeignName(tsTeacherInfoId);
+    }
 
-	/**
-	* 根据ID查询指定的老师信息管理(包含老师信息管理和外键名称)
-	*
-	* @param tsTeacherInfoId Id
-	*/
-	@PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:view')")
-	@ApiOperation("根据ID查询指定的老师信息管理(包含老师信息管理和外键名称)")
-	@PostMapping("/findTsTeacherInfoForEdit")
-	public TsTeacherInfoEditDto findTsTeacherInfoForEdit(@RequestParam Long tsTeacherInfoId){
-				TsTeacherInfoEditDto tsTeacherInfoEditDto = new TsTeacherInfoEditDto();
-		tsTeacherInfoEditDto.setTsTeacherInfo(this.tsTeacherInfoService.findTsTeacherInfoWithForeignName(tsTeacherInfoId));
+    /**
+     * 根据ID查询指定的老师信息管理(包含老师信息管理和外键名称)
+     *
+     * @param tsTeacherInfoId Id
+     */
+    @PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:view')")
+    @ApiOperation("根据ID查询指定的老师信息管理(包含老师信息管理和外键名称)")
+    @PostMapping("/findTsTeacherInfoForEdit")
+    public TsTeacherInfoEditDto findTsTeacherInfoForEdit(@RequestParam Long tsTeacherInfoId) {
+        TsTeacherInfoEditDto tsTeacherInfoEditDto = new TsTeacherInfoEditDto();
+        tsTeacherInfoEditDto.setTsTeacherInfo(this.tsTeacherInfoService.findTsTeacherInfoWithForeignName(tsTeacherInfoId));
 
-		this.prepareTsTeacherInfoEditDto(tsTeacherInfoEditDto);
+        this.prepareTsTeacherInfoEditDto(tsTeacherInfoEditDto);
 
-		return tsTeacherInfoEditDto;
-	}
+        return tsTeacherInfoEditDto;
+    }
 
-	/**
-	* 根据ID查询指定的老师信息管理(只提取ID 和 Name)
-	*
-	* @param tsTeacherInfoId Id
-	*/
-	@PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:view')")
-	@ApiOperation("根据ID查询指定的老师信息管理(只提取ID 和 Name)")
-	@PostMapping("/findTsTeacherInfosWithIdNameById")
-	public TsTeacherInfo findTsTeacherInfosWithIdNameById(@RequestParam Long tsTeacherInfoId){
-	return this.tsTeacherInfoService.findTsTeacherInfosWithIdNameById(tsTeacherInfoId);
-	}
+    /**
+     * 根据ID查询指定的老师信息管理(只提取ID 和 Name)
+     *
+     * @param tsTeacherInfoId Id
+     */
+    @PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:view')")
+    @ApiOperation("根据ID查询指定的老师信息管理(只提取ID 和 Name)")
+    @PostMapping("/findTsTeacherInfosWithIdNameById")
+    public TsTeacherInfo findTsTeacherInfosWithIdNameById(@RequestParam Long tsTeacherInfoId) {
+        return this.tsTeacherInfoService.findTsTeacherInfosWithIdNameById(tsTeacherInfoId);
+    }
 
-	/**
-	* 根据名称查询老师信息管理集合(只提取ID 和 Name)
-	*
-	* @param tsTeacherInfoName 名称
-	*/
-	@PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:view')")
-	@ApiOperation("根据名称查询老师信息管理集合(只提取ID 和 Name)")
-	@PostMapping("/findTsTeacherInfosWithIdNameByName")
-	public List<TsTeacherInfo> findTsTeacherInfosWithIdNameByName(@RequestParam String tsTeacherInfoName){
-	return this.tsTeacherInfoService.findTsTeacherInfosWithIdNameByName(tsTeacherInfoName);
-	}
+    /**
+     * 根据名称查询老师信息管理集合(只提取ID 和 Name)
+     *
+     * @param tsTeacherInfoName 名称
+     */
+    @PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:view')")
+    @ApiOperation("根据名称查询老师信息管理集合(只提取ID 和 Name)")
+    @PostMapping("/findTsTeacherInfosWithIdNameByName")
+    public List<TsTeacherInfo> findTsTeacherInfosWithIdNameByName(@RequestParam String tsTeacherInfoName) {
+        return this.tsTeacherInfoService.findTsTeacherInfosWithIdNameByName(tsTeacherInfoName);
+    }
 
 
-	/**
-	* 创建新的老师信息管理
-	*
-	*/
-	@PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:add')")
-	@ApiOperation("创建新的老师信息管理")
-	@PostMapping("/createTsTeacherInfo")
-	public TsTeacherInfoEditDto createTsTeacherInfo(){
-				TsTeacherInfoEditDto tsTeacherInfoEditDto = new TsTeacherInfoEditDto();
-		tsTeacherInfoEditDto.setTsTeacherInfo(new TsTeacherInfo());
+    /**
+     * 创建新的老师信息管理
+     */
+    @PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:add')")
+    @ApiOperation("创建新的老师信息管理")
+    @PostMapping("/createTsTeacherInfo")
+    public TsTeacherInfoEditDto createTsTeacherInfo() {
+        TsTeacherInfoEditDto tsTeacherInfoEditDto = new TsTeacherInfoEditDto();
+        tsTeacherInfoEditDto.setTsTeacherInfo(new TsTeacherInfo());
 
-		this.prepareTsTeacherInfoEditDto(tsTeacherInfoEditDto);
-		return tsTeacherInfoEditDto;
-	}
+        this.prepareTsTeacherInfoEditDto(tsTeacherInfoEditDto);
+        return tsTeacherInfoEditDto;
+    }
 
-	private void prepareTsTeacherInfoEditDto(TsTeacherInfoEditDto tsTeacherInfoEditDto){
-																		        //TODO: 以下代码可以注释掉，此行代码即时加载所有外键对象，以便选择。如果不在此加载，可以在界面上实现延迟加载。如果外键对象超过 500 行，建议采用延迟加载
-		tsTeacherInfoEditDto.setCollegeTsColleges(this.tsCollegeService.findAllTsCollegesWithIdName());
-											        //TODO: 以下代码可以注释掉，此行代码即时加载所有外键对象，以便选择。如果不在此加载，可以在界面上实现延迟加载。如果外键对象超过 500 行，建议采用延迟加载
-		tsTeacherInfoEditDto.setPositionTsPositions(this.tsPositionService.findAllTsPositionsWithIdName());
-																			}
+    private void prepareTsTeacherInfoEditDto(TsTeacherInfoEditDto tsTeacherInfoEditDto) {
+        //TODO: 以下代码可以注释掉，此行代码即时加载所有外键对象，以便选择。如果不在此加载，可以在界面上实现延迟加载。如果外键对象超过 500 行，建议采用延迟加载
+        tsTeacherInfoEditDto.setCollegeTsColleges(this.tsCollegeService.findAllTsCollegesWithIdName());
+        //TODO: 以下代码可以注释掉，此行代码即时加载所有外键对象，以便选择。如果不在此加载，可以在界面上实现延迟加载。如果外键对象超过 500 行，建议采用延迟加载
+        tsTeacherInfoEditDto.setPositionTsPositions(this.tsPositionService.findAllTsPositionsWithIdName());
+    }
 
-	/**
-	* 新增保存老师信息管理
-	*
-	* @param tsTeacherInfo 实体对象
-	*/
-	@PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:add')")
-	@ApiOperation("新增保存老师信息管理")
-	@PostMapping("/saveTsTeacherInfo")
-	public TsTeacherInfo saveTsTeacherInfo(@RequestBody TsTeacherInfo tsTeacherInfo){
-		return this.tsTeacherInfoService.saveTsTeacherInfo(tsTeacherInfo);
-	}
+    /**
+     * 新增保存老师信息管理
+     *
+     * @param tsTeacherInfo 实体对象
+     */
+    @PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:add')")
+    @ApiOperation("新增保存老师信息管理")
+    @PostMapping("/saveTsTeacherInfo")
+    public TsTeacherInfo saveTsTeacherInfo(@RequestBody TsTeacherInfo tsTeacherInfo) {
+        return this.tsTeacherInfoService.saveTsTeacherInfo(tsTeacherInfo);
+    }
 
-	/**
-	* 修改保存老师信息管理
-	*
-	* @param tsTeacherInfo 实体对象
-	*/
-	@PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:update')")
-	@ApiOperation("修改保存老师信息管理")
-	@PostMapping("/updateTsTeacherInfo")
-	public TsTeacherInfo updateTsTeacherInfo(@RequestBody TsTeacherInfo tsTeacherInfo){
-		return this.tsTeacherInfoService.updateTsTeacherInfo(tsTeacherInfo);
-	}
+    /**
+     * 修改保存老师信息管理
+     *
+     * @param tsTeacherInfo 实体对象
+     */
+    @PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:update')")
+    @ApiOperation("修改保存老师信息管理")
+    @PostMapping("/updateTsTeacherInfo")
+    public TsTeacherInfo updateTsTeacherInfo(@RequestBody TsTeacherInfo tsTeacherInfo) {
+        return this.tsTeacherInfoService.updateTsTeacherInfo(tsTeacherInfo);
+    }
 
-	/**
-	* 根据ID删除老师信息管理
-	*
-	* @param tsTeacherInfoId ID
-	*/
-	@PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:remove')")
-	@ApiOperation("根据ID删除老师信息管理")
-	@PostMapping("/deleteTsTeacherInfo")
-	public void deleteTsTeacherInfo(@RequestParam Long tsTeacherInfoId){
-		this.tsTeacherInfoService.deleteTsTeacherInfo(tsTeacherInfoId);
-	}
-
+    /**
+     * 根据ID删除老师信息管理
+     *
+     * @param tsTeacherInfoId ID
+     */
+    @PreAuthorize("hasAuthority('tms:tsteacherEntity:TsTeacherInfo:remove')")
+    @ApiOperation("根据ID删除老师信息管理")
+    @PostMapping("/deleteTsTeacherInfo")
+    public void deleteTsTeacherInfo(@RequestParam Long tsTeacherInfoId) {
+        this.tsTeacherInfoService.deleteTsTeacherInfo(tsTeacherInfoId);
+    }
 
 
 }
